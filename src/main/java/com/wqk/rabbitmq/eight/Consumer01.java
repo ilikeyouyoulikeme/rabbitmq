68f5cd4708bc1f00cc0a488c23becf1722c96d50
wqk <2393443373@qq.com>
2022-08-14 15:59:33 +0800

    consumer11

---
+++ src/main/java/com/wqk/rabbitmq/eight/Consumer01.java
@@ -0,0 +1,13 @@
+/* 王其康版权所有
+ */
+package com.wqk.rabbitmq.eight;
+
+/**
+ * @author wangqikang
+ * @version 1.0
+ */
+public class Consumer01 {
+    public static final String NORMAL_QUEUE = "normal_queue";
+
+
+}
